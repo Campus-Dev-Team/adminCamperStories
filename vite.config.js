8c98d90d98ba79d78dca124dfbc20701785f3b1e
himayrobsarr <serganimon@gmail.com>
2025-02-04 17:09:48 -0500

    fix: :hammer: "pequeno fix"

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   },
-  server: {
-    port: 3000
+  build: {
+    outDir: 'dist'
   }
-})
+});
